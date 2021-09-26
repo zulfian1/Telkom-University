cfb6524aafe9855e7d795debecb39120b1ec269d
zulfian <fuadn440@gmail.com>
2021-09-26 21:53:40 +0700

    HelloWorld.java

---
+++ Telkom University 2/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Nama: Fuad Nazri Dzulfian Firdaus");
+		System.out.println("NIM: 6706213035");
+		System.out.println("Kelas: D3RPLA04");
+	}
+} 
